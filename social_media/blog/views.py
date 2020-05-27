f5e11861a9704ebb2175faf494e29674ddb3c051
Sudarshan <sudarshansharmano.1@gmail.com>
2020-05-27 23:01:58 +0530

    Blog App added

--- social_media/blog/views.py
+++ social_media/blog/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+posts=[
+    {
+        'author':'Sud',
+        'title':'Post 1',
+        'content':'First post of the day',
+        'date_posted':'16th Aug'
+    },
+    {
+        'author':'Radhika',
+        'title':'Post 2',
+        'content':'Second post of the day',
+        'date_posted':'17th Aug'
+    },
+
+
+]
 # Create your views here.
 def home(request):
-    return render(request,'blog/home.html')
+    data={'posts':posts}
+    return render(request,'blog/home.html',data)
 
 def about(request):
     return render(request,'blog/about.html')
