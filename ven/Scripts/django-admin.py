0466a1af13c91325f9bb514b56cbeaaa5d8689ee
Sudarshan <sudarshansharmano.1@gmail.com>
2020-05-27 22:34:32 +0530

    Blog app added

---
+++ ven/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\asus\pycharmprojects\social_media_website\ven\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
