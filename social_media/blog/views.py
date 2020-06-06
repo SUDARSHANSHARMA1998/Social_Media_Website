64a950971e9a69232af7d48912620db38ea8ba8b
Sudarshan <sudarshansharmano.1@gmail.com>
2020-06-07 04:51:31 +0530

    ViewList + DetailList

--- social_media/blog/views.py
+++ social_media/blog/views.py
@@ -1,25 +1,21 @@
 from django.shortcuts import render
+from django.views.generic import ListView,DetailView
 from .models import Post
-posts=[
-    {
-        'author':'Sud',
-        'title':'Post 1',
-        'content':'First post of the day',
-        'date_posted':'16th Aug'
-    },
-    {
-        'author':'Radhika',
-        'title':'Post 2',
-        'content':'Second post of the day',
-        'date_posted':'17th Aug'
-    },
 
 
-]
 # Create your views here.
 def home(request):
     data={'posts':Post.objects.all()}
     return render(request,'blog/home.html',data)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
 
 def about(request):
     return render(request,'blog/about.html',{'title':'About us'})
